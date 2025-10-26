2800a4fe3de6f20dd47418003b7638c1743d9120
shizz4lol <rzzutoambar3@gmail.com>
2025-10-26 06:54:16 -0300

    Verificacion de inputs vacios

--- WEB/JSformulario.js
+++ WEB/JSformulario.js
@@ -1,24 +1,27 @@
-function vacio(){
-    $inputs = document.getElementsByClassName("campo");
-    var $v;
-    /* $validacion= Array(); */
-    for ($i=0;$i<$inputs.length;$i++){
-        if ($inputs[$i]==""){
-            $v=1;
-        }}
-        /* else{
-            $v=0;
-        }
-        $validacion[$i]=$v;
-    } */
-    /* return $validacion; */
-}
-/* function vacio_muestra(){
-    $val=vacio();
-    $w =document.getElementsByName("parrafo");
-    for ($i=0;$i<$val.length;$i++){
-        if ($val[$i]==1){
-            console.log("HOLA");
-        }
+function verif_vacio(event) {
+    //metodo para evitar el default: re-envio del formulario
+    event.preventDefault(); 
+    const campos = document.getElementsByClassName("campo");
+    const p_e = document.getElementById("exito");
+    const p_warning = document.getElementsByName("warning");
+    var cant_vacios = Array();
+    p_e.innerHTML = ""; 
+    let vacios = 0; 
+    for (let i = 0; i < campos.length; i++) {
+      if (campos[i].value.trim() === "") {
+        vacios++;
+        cant_vacios[i]=vacios;
+      } else {
+        campos[i].style.border = "";
+      }
     }
-} */
+    for (i in cant_vacios){
+        p_warning[i].innerHTML+= "Campo vacio <br>";
+    }
+    if (vacios == 0) {
+      p_e.innerHTML = "Envio exitoso!";
+      return true; // podrías permitir el envío si querés
+    }
+  
+    return false; // evita el envío si hay vacíos
+  }
